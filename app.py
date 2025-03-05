eeb69e3d672f3b0b2caf8c015bc18acafec43cf3
Evgenii G <sudodebug@yandex.ru>
2025-03-05 21:53:50 +0500

    Create func get_tables (read table list to load)

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 import sys
 from config import DB_DETAILS
+from utils import get_tables
 
 
 def main():
     """Program takes al least one argument"""
     env = sys.argv[1]
     db_details = DB_DETAILS[env]
-    print(db_details)
+    tables = get_tables('table_list.txt')
+    for table in tables['table_name']:
+        print(table)
 
 
 if __name__ == '__main__':
